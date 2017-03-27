d09e12eb48cfbc777165e8e972da87f07e2cecad
Silvano Amaral <silvano.amaral@pmweb.com.br>
2017-03-27 14:26:36 -0300

    arquivos

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ var App = {
             v=v.replace(/D/g,"");             //Remove tudo o que não é dígito
             v=v.replace(/^(d{2})(d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
             v=v.replace(/(d)(d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
-            return v;
+            return v; 
         }
     },
     handlers: {
