3ad5a52d2c1ac74e06591680aeb707e15c17def6
Silvano Amaral <silvano.amaral@pmweb.com.br>
2017-03-27 13:26:05 -0300

    adicionando funcão para mascara.

--- script.js
+++ script.js
@@ -55,6 +55,18 @@ var App = {
         getRandomArbitrary: function(min, max) {
             /* gerar um número randômico */
             return (Math.random() * (max - min) + min).toFidex();
+        },
+        telefone : function(v){
+            v=v.replace(/\D/g,"");               //Remove tudo o que não é dígito
+            v=v.replace(/^(\d\d)(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+            v=v.replace(/(\d{4})(\d)/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígito
+            return v;
+        },
+        mtel : function(v){
+            v=v.replace(/D/g,"");             //Remove tudo o que não é dígito
+            v=v.replace(/^(d{2})(d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+            v=v.replace(/(d)(d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+            return v;
         }
     },
     handlers: {
